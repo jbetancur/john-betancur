80ab3a899b881cddb84fd9b0f0ebc136842b6c2e
John Betancur <johnnyazee@gmail.com>
2019-08-20 15:39:48 -0400

    more style fixes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 // custom typefaces
 // import "typeface-montserrat"
-import './src/styles/global.css';
+import './src/styles/global.scss';
 import 'prismjs/themes/prism-tomorrow.css';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 import  './src/styles/prism-override.scss';
